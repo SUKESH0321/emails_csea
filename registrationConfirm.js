a541deec4e2e8c22654c9e0cf2595ae4abf5004f
SUKESH0321 <sukesh0321@gmail.com>
2026-01-06 19:19:12 +0530

    Initial commit

---
+++ registrationConfirm.js
@@ -0,0 +1,122 @@
+export const registrationConfirmTemplate = (
+    name,
+    kriyaId,
+    headerImg = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dio4yY3bwPx8&psig=AOvVaw3BimO4yoSIuC1KBLmx1-Q_&ust=1737710321981000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDrycXBi4sDFQAAAAAdAAAAABAH",
+    footerImg = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dio4yY3bwPx8&psig=AOvVaw3BimO4yoSIuC1KBLmx1-Q_&ust=1737710321981000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDrycXBi4sDFQAAAAAdAAAAABAH"
+) => ` <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tbody>
+            <tr>
+                <td bgcolor="#f5f5f5" align="center" style="padding: 0px 15px 18px 15px; background:#eeceff">
+                    <table border="0" cellpadding="0" cellspacing="0" width="666" class="responsive-table">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                            <tr>
+                                                <td align="center"
+                                                    style="padding: 18px 0 0 0; border-top: 1px #d3cfd3 solid;">
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <!-- HERO IMAGE -->
+                                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <a href="" target="_blank">
+
+                                                        <img src="https://i.ibb.co/tpHnkWkw/EMAIL-HEADER.jpg" width="666"
+                                                            border="0" alt="Kriya 2025 Registration Cover"
+                                                            style="display: block; color: #666666;  font-family: Helvetica, arial, sans-serif; font-size: 16px;"
+                                                            class="img-max">
+
+
+
+                                                    </a>
+                                                    <!-- src="https://i.ibb.co/qknwSd6/Pastel-Rainbow-Modern-January-Monthly-Email-Header-1.png" -->
+
+                                                </td>
+                                            </tr>
+                                            <tr style="background:white;margin-top: 2rem;">
+                                                <td align="center">
+                                                    <p
+                                                        style="font-size: 2rem; font-weight: 600; font-family: 'Poppins', sans-serif; color: black;padding-top: 1rem;">
+                                                        Thank you for registering!
+                                                    </p>
+                                                </td>
+                                            </tr>
+                                            <tr style="background:white;">
+                                                <td
+                                                    style=" padding: 0px 40px 0px 2rem; font-size: 1.1rem;  font-family: 'Poppins', sans-serif;color:black;">
+                                                    <b style=" font-size: 1.2rem;">
+                                                        Hey ${name},
+                                                    </b>
+                                                    <p>
+                                                        Congratulations on successfully registering for Kriya 2025! We’re thrilled to have you join us. Your Kriya ID for the event is:
+                                                    </p>
+                                                    <p style="font-size: 2rem; color:#470067; font-weight: bold;">
+                                                        ${kriyaId}
+                                                    </p>
+                                                    <p>
+                                                        Follow our website for the latest updates on exciting workshops
+                                                        and events
+                                                    </p>
+                                                    <b>Best Wishes,</b>
+                                                    <p style="padding-bottom: 4rem;">
+                                                        Kriya 2025 Team<br />
+                                                        PSG College of Technology
+                                                    </p>
+
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <a href="" target="_blank">
+
+                                                        <img src="https://i.ibb.co/hkQmjsB/EMAIL-FOOTER.jpg" width="666"
+                                                            height="130" border="0" alt="Kriya 2024 Registration Cover"
+                                                            style="display: block; color: #666666;  font-family: Helvetica, arial, sans-serif; font-size: 16px;"
+                                                            class="img-max">
+
+
+
+                                                    </a>
+
+
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+        <tbody>
+            <tr>
+                <td bgcolor="#ffffff" align="center" style="padding:  0;">
+                    <table width="666" border="0" cellspacing="0" cellpadding="0" align="center"
+                        class="responsive-table">
+                        <tbody>
+                            <tr>
+                                <td align="center"
+                                    style="padding: 1rem;    font-size: 12px; line-height: 18px; font-family: Helvetica, Arial, sans-serif; color:#666666;">
+                                    <span class="appleFooter" style="color:#666666;">Students Union - PSG College of
+                                        Technology
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+`;
